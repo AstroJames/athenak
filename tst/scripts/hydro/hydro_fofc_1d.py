4ca220425d300ff34f0355cf031527ec72597a32
James Beattie <james.beattie@princeton.edu>
2026-08-18 23:35:29 -0400

    Add TENO6 and TENO6-opt reconstruction

--- tst/scripts/hydro/hydro_fofc_1d.py
+++ tst/scripts/hydro/hydro_fofc_1d.py
@@ -11,7 +11,7 @@ import scripts.utils.athena as athena
 logger = logging.getLogger('athena' + __name__[7:])
 
 _INPUT = 'hydro/sod.athinput'
-_RECONSTRUCTIONS = ('plm', 'wenoz', 'teno5', 'teno5_opt')
+_RECONSTRUCTIONS = ('plm', 'wenoz', 'teno5', 'teno5_opt', 'teno6', 'teno6_opt')
 
 
 def run(**kwargs):
